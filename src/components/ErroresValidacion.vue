8fd2e8593801de5d4d7ad6eea4824b27e5a4d341
Williams Santos <willysantos_lai@gmail.com>
2019-12-03 21:35:38 -0600

    formulario ciudad validado y confirmacion

---
+++ src/components/ErroresValidacion.vue
@@ -0,0 +1,17 @@
+<template>
+    <vs-list v-if="errors.length">
+        <vs-list-header title="Errores" color="danger"></vs-list-header>
+        <vs-list-item icon="clear" color="danger" v-for="error in errors" :title="error">
+        </vs-list-item>
+    </vs-list>
+</template>
+
+<script>
+    export default {
+        name: "ErroresValidacion"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Formularios/Creacion/Ciudad.vue
+++ src/components/Formularios/Creacion/Ciudad.vue
@@ -1,36 +1,43 @@
 <template>
-    <vs-row vs-justify="center">
-        <vs-col type="flex" class="tarjetas" vs-justify="center" vs-align="center" vs-w="3">
-            <vs-card actionable class="cardx">
-                <div slot="header">
-                    <h3>
-                        Creacion de ciudades
-                    </h3>
-                </div>
-                <form
-                    action="#"
-                    >
-                    <div>
+    <div>
+        <vs-row vs-justify="center">
+            <vs-col type="flex" class="tarjetas" vs-justify="center" vs-align="center" vs-w="3">
+                <vs-card actionable class="cardx">
+                    <div slot="header">
+                        <h3>
+                            Creacion de ciudades
+                        </h3>
+                    </div>
+                        <div>
+                            <div class="centerx">
+                                <vs-input label-placeholder="Nombre de la Ciudad" v-model="NombreCiudad" type="text" id="NombreCiudad"/>
+                            </div>
 
-                        <div class="centerx">
-                            <vs-input label-placeholder="Nombre de la Ciudad" v-model="NombreCiudad"/>
+                        </div>
+                        <div slot="footer">
+                            <vs-row vs-justify="flex-end">
+                                <vs-button @click="checkForm" color="success" type="filled" icon="done">Guardar Equipo</vs-button>
+                            </vs-row>
                         </div>
 
-                    </div>
-                    <div slot="footer">
-                        <vs-row vs-justify="flex-end">
-                            <vs-button color="success" type="filled" icon="done">Guardar Ciudad</vs-button>
-                        </vs-row>
-                    </div>
-                </form>
-            </vs-card>
-        </vs-col>
-        <vs-col type="flex" class="tarjetas" vs-justify="center" vs-align="center" vs-w="5">
-            <vs-card actionable class="cardx">
-                <TablaCiudades/>
-            </vs-card>
-        </vs-col>
-    </vs-row>
+                </vs-card>
+<!--                errores de validacion-->
+                <vs-list v-if="errors.length">
+                    <vs-list-header title="Errores" color="danger"></vs-list-header>
+                    <vs-list-item icon="clear" color="danger" v-for="error in errors" :title="error">
+                    </vs-list-item>
+                </vs-list>
+<!--                aca terminan los errores-->
+            </vs-col>
+            <vs-col type="flex" class="tarjetas" vs-justify="center" vs-align="center" vs-w="5">
+                <vs-card actionable class="cardx">
+                    <TablaCiudades/>
+                </vs-card>
+
+            </vs-col>
+        </vs-row>
+    </div>
+
 </template>
 
 <script>
@@ -38,10 +45,43 @@
     export default {
         name: "Ciudad",
         components:{TablaCiudades},
-        data(){
-            return {
-                NombreCiudad:'',
+        data:()=> {
+            return{
+                errors: [],
+                NombreCiudad: null,
             }
+        },
+        methods:{
+            checkForm: function () {
+            if (this.NombreCiudad && this.NombreCiudad.length > 2){
+                this.EnvioDatos()
+            }
+            this.errors = [];
+            if (!this.NombreCiudad){
+                this.errors.push("El campo nombre no debe estar vacio")
+            }
+            if (this.NombreCiudad.length < 3){
+                this.errors.push("El campo nombre debe ser mayor a dos caracteres")
+            }
+        },
+            EnvioDatos: function () {
+                this.openConfirm()
+            },
+            openConfirm(){
+                this.$vs.dialog({
+                    color: 'success',
+                    title: `Guardado`,
+                    text: 'Se ha guardado exitosamente la ciudad '+this.NombreCiudad+".",
+                    accept:this.acceptAlert
+                })
+            },
+            acceptAlert(color){
+                this.$vs.notify({
+                    color:'success',
+                    title:'Guardado',
+                    text:'Se guardo la ciudad '+this.NombreCiudad+"."
+                })
+            },
         }
     }
 </script>
