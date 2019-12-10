847b7e47bd81f830eedfe32091ff285710476530
Ivan-godoy <ivangodoy24@gmail.com>
2019-12-09 20:30:08 -0600

    Validacion del formulario

--- src/view/Login.vue
+++ src/view/Login.vue
@@ -6,25 +6,25 @@
                 <div class="container  h-100 w-100">
                     <div class="row  h-100">
                         <div class="col align-self-center">
-                            <form class="border p-3 shadow-lg p-3 mb-5 rounded" id="Login" @submit="checkForm" action="Login.vue" method="post">
+                            <form class="border p-3 shadow-lg p-3 mb-5 rounded align-items-center" id="Login" @submit="checkForm" action="Login.vue" method="post">
                                 <div class="row form-group  justify-content-center align-items-center">
                                     <img src="../img/logo.png" alt="" class="w-50 h-25">
                                 </div>
                                 <div class="form-group">
-                                    <label for="usuario">Email address</label>
-                                    <input v-model="usuario" type="text" class="form-control" id="usuario" aria-describedby="IngresoUsuario" placeholder="Usuario">
+                                    <label for="usuario">Nombre de Usuario</label>
+                                    <input v-model="usuario" type="text" class="form-control" v-bind:class="['form-control', validaU]"  id="usuario" aria-describedby="IngresoUsuario" placeholder="Usuario"  @keyup="validacionU()">
                                     <small v-for="error in errorsU" class="form-text text-muted"> {{ error }}</small>
                                 </div>
                                 <div class="form-group">
-                                    <label for="clave">Password</label>
-                                    <input v-model="clave" type="password" class="form-control" id="clave" placeholder="Password">
+                                    <label for="clave">Contraseña</label>
+                                    <input v-model="clave" type="password" class="form-control" v-bind:class="['form-control', validaC]" id="clave" placeholder="Contraseña" @keyup="validacionC()">
                                     <small v-for="error in errorsC" class="form-text text-muted"> {{ error }}</small>
                                 </div>
                                 <div class="form-group form-check">
                                     <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                    <label class="form-check-label" for="exampleCheck1">Recordar Credenciales</label>
                                 </div>
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <button type="submit" class="btn btn-success">Ingresar</button>
                             </form>
                         </div>
                     </div>
@@ -46,23 +46,42 @@
                 usuario: null,
                 clave: null,
                 col: null,
+                validaU: null,
+                validaC: null,
                 bg: 'bg-light'
             }
         },
         methods:{
+            validacionU: function(){
+                this.errorsU = [];
+                if (!this.usuario) {
+                    this.errorsU.push('El usuario es obligatorio');
+                    this.validaU = 'is-invalid';
+                }
+                if(this.usuario !== "root"){
+                    this.errorsU.push('El nombre de usuario no existe');
+                    this.validaU = 'is-invalid';
+                }else{
+                    this.validaU = 'is-valid';
+                }
+            },
+            validacionC: function(){
+                this.errorsC = [];
+                if (!this.clave) {
+                    this.errorsC.push('La contraseña es obligatoria.');
+                    this.validaC = 'is-invalid';
+                }
+                if(this.clave !== "contra"){
+                    this.errorsC.push('La contraseña es incorrecta');
+                    this.validaC = 'is-invalid';
+                }
+                else{
+                    this.validaC = 'is-valid'
+                }
+            },
             checkForm: function (e) {
                 if (this.usuario && this.clave) {
                     return true;
-                }
-                this.errorsU = [];
-                this.errorsC = [];
-                if (!this.usuario) {
-                    this.errorsU.push('El nombre es obligatorio.');
-                    console.log('hola');
-                }
-                if (!this.clave) {
-                    this.errorsC.push('La edad es obligatoria.');
-                    console.log('holac');
                 }
                 e.preventDefault();
             }
