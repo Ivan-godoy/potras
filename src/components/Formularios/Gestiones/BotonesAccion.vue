34ec63608cbcd8964c62acfde7e5b4acc2f8ae8a
OREL <orelenriquez19@gmail.com>
2019-12-09 20:36:54 -0600

    Cambios en Pestaña de Equipo

--- src/components/Formularios/Gestiones/BotonesAccion.vue
+++ src/components/Formularios/Gestiones/BotonesAccion.vue
@@ -1,13 +1,45 @@
 <template lang="html">
     <div class="centerx">
-        <vs-button @click="popupActivo=true" vs-type="gradient" size="medium" color="success" icon="create" style="margin-right: 10px"></vs-button>
-        <vs-popup class="holamundo"  title="Boton para Editar" :active.sync="popupActivo">
-<p>Hola Guapo</p>
-        </vs-popup>
-        <vs-button @click="popupActivo=true" vs-type="flat" size="medium" color="danger" icon="delete_sweep"></vs-button>
-        <vs-popup class="holamundo"  title="Boton para Eliminar" :active.sync="popupActivo">
-            <p>Hola Guapo</p>
-        </vs-popup>
+        <vs-col :key="index" vs-type="flex" vs-justify="center" vs-align="center" vs-w="10">
+            <vs-row vs-justify="center">
+                <vs-col type="flex" class="tarjetas" vs-justify="center" vs-align="center" vs-w="50">
+                    <vs-card actionable class="cardx" >
+                        <div slot="header">
+                            <h3>Edición de Equipos</h3>
+                        </div>
+                        <div class="centerx default-input">
+                            <div class="contenedor">
+                                <h6>Nombre del Equipo</h6>
+                                <!--<vs-input class="inputx" label-placeholder="Nombre del Equipo" v-model="NombreEquipo"/>-->
+                                <vs-select autocomplete class="selectExample1" v-model="SelectNombre">
+                                    <vs-select-item :key="index1" :value="item1.value1" :text="item1.text1" v-for="(item1,index1) in options11" />
+                                </vs-select>
+                                <h6>Esquema Habitual</h6>
+                                <vs-input class="inputx"  label-placeholder="Esquema Habitual" v-model="Esquema"/>
+                                <h6>Ciudad</h6>
+                                <vs-select autocomplete class="selectExample" v-model="SelectCiudad">
+                                    <vs-select-item :key="index" :value="item.value" :text="item.text" v-for="(item,index) in options1" />
+                                </vs-select>
+                                <h6>Fecha de Fundación</h6>
+                                <vs-input type="date" class="inputx"  v-model="FechaFundacion"/>
+                            </div>
+                            <div class="contenedor">
+                                <div class="centerx">
+                                    <h6 class="text-center">Subir logo</h6>
+                                    <vs-upload action="https://jsonplaceholder.typicode.com/posts/" @on-success="successUpload" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div slot="footer">
+                            <vs-row vs-justify="flex-end">
+                                <vs-button @click="checkForm" color="success" type="filled" icon="done">Editar Equipo</vs-button>
+                            </vs-row>
+                        </div>
+                    </vs-card>
+                </vs-col>
+            </vs-row>
+        </vs-col>
     </div>
 </template>
 
--- src/components/Formularios/Gestiones/Equipo.vue
+++ src/components/Formularios/Gestiones/Equipo.vue
@@ -59,44 +59,7 @@
                                     <vs-col :key="index" vs-type="flex" vs-justify="center" vs-align="center" vs-w="10">
                                         <vs-button @click="popupActivo=true"  vs-type="gradient" size="medium" color="success" icon="create" style="margin-right: 10px"></vs-button>
                                         <vs-popup class="gestion_Equipo"  title="Editar Equipo" :active.sync="popupActivo">
-                                            <vs-row vs-justify="center">
-                                                <vs-col type="flex" class="tarjetas" vs-justify="center" vs-align="center" vs-w="50">
-                                                    <vs-card actionable class="cardx" >
-                                                        <div slot="header">
-                                                            <h3>Edición de Equipos</h3>
-                                                        </div>
-                                                        <div class="centerx default-input">
-                                                            <div class="contenedor">
-                                                                <h6>Nombre del Equipo</h6>
-                                                                <!--<vs-input class="inputx" label-placeholder="Nombre del Equipo" v-model="NombreEquipo"/>-->
-                                                                <vs-select autocomplete class="selectExample1" v-model="SelectNombre">
-                                                                    <vs-select-item :key="index1" :value="item1.value1" :text="item1.text1" v-for="(item1,index1) in options11" />
-                                                                </vs-select>
-                                                                <h6>Esquema Habitual</h6>
-                                                                <vs-input class="inputx"  label-placeholder="Esquema Habitual" v-model="Esquema"/>
-                                                                <h6>Ciudad</h6>
-                                                                <vs-select autocomplete class="selectExample" v-model="SelectCiudad">
-                                                                    <vs-select-item :key="index" :value="item.value" :text="item.text" v-for="(item,index) in options1" />
-                                                                </vs-select>
-                                                                <h6>Fecha de Fundación</h6>
-                                                                <vs-input type="date" class="inputx"  v-model="FechaFundacion"/>
-                                                            </div>
-                                                            <div class="contenedor">
-                                                                <div class="centerx">
-                                                                    <h6 class="text-center">Subir logo</h6>
-                                                                    <vs-upload action="https://jsonplaceholder.typicode.com/posts/" @on-success="successUpload" />
-                                                                </div>
-                                                            </div>
-                                                        </div>
 
-                                                        <div slot="footer">
-                                                            <vs-row vs-justify="flex-end">
-                                                                <vs-button @click="checkForm" color="success" type="filled" icon="done">Editar Equipo</vs-button>
-                                                            </vs-row>
-                                                        </div>
-                                                    </vs-card>
-                                                </vs-col>
-                                            </vs-row>
                                         </vs-popup>
                                         <vs-button @click="popupActivo1=true" vs-type="flat" size="medium" color="danger" icon="delete_sweep"></vs-button>
                                         <vs-popup class="gesion_Equipos"  title="Eliminar Equipo" :active.sync="popupActivo1">
@@ -115,6 +78,7 @@
 </template>
 
 <script>
+
     export default {
         name: "Equipo",
         data(){
@@ -149,6 +113,7 @@
                 popupActivo1:false
             }
         },
+
     }
 </script>
 
