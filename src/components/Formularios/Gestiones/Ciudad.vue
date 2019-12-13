e7d1beb8dfde80f03cf17b41507bab8fe1330f89
EdgardoArgueta29 <19992909tato>
2019-12-13 02:16:37 -0600

    Tabla Ciudades en Gestiones y Activacion del boton de modificar

--- src/components/Formularios/Gestiones/Ciudad.vue
+++ src/components/Formularios/Gestiones/Ciudad.vue
@@ -1,68 +1,88 @@
-<template lang="html">
-  <div>
-    <vs-table max-items="5" pagination :data="ciudades">
-      <template slot="header">
-        <h3>
-          Ciudades
-        </h3>
-      </template>
-      <template slot="thead">
-        <vs-th>
-          Nombre Ciudad
-        </vs-th>
-        <vs-th>
-          Accciones
-        </vs-th>
-      </template>
+<template>
+  <vs-row vs-justify="center">
+    <vs-col type="flex" class="tarjetas" vs-justify="center" vs-align="center" vs-w="10">
+      <div>
+        <vs-table stripe :data="ciudades">
+          <template slot="header">
+            <h3>
+              Ciudad
+            </h3>
+          </template>
+          <template slot="thead">
+            <vs-th>
+              Nombre
+            </vs-th>
 
-      <template>
-        <vs-tr :key="indextr" v-for="(indextr) in ciudades" >
-          <vs-td :data="indextr.nombre">
-            {{indextr.nombre}}
-          </vs-td>
-        </vs-tr>
-      </template>
-    </vs-table>
-  </div>
+            <vs-th>
+              Acciones
+            </vs-th>
+          </template>
+          <template slot-scope="{data}">
+            <vs-tr :key="indextr" v-for="(indextr) in ciudades" >
+              <vs-td :data="indextr.nombre">
+                {{indextr.nombre}}
+              </vs-td>
+
+              <vs-td>
+                <vs-row vs-w="12">
+                  <div>
+                    <vs-col :key="index" vs-type="flex" vs-justify="center" vs-align="center" vs-w="10">
+                      <vs-button @click="popupActivo=true"  vs-type="gradient" size="medium" color="success" icon="create" style="margin-right: 10px"></vs-button>
+                      <vs-popup class="gestion_Ciudades"  title="Editar Ciudad" :active.sync="popupActivo">
+                        <div class="contenedor">
+                          <FormCiudad titulo="Formulario Ciudades" :id_equipo="id"/>
+                        </div>
+                      </vs-popup>
+                      <vs-button @click="popupActivo1=true" vs-type="flat" size="medium" color="danger" icon="delete_sweep"></vs-button>
+                      <vs-popup class="gestion_Ciudad"  title="Eliminar Ciudad" :active.sync="popupActivo1">
+                        <p>Hola Guapo, ¿vas a eliminarme?</p>
+                      </vs-popup>
+                    </vs-col>
+                  </div>
+                </vs-row>
+              </vs-td>
+            </vs-tr>
+          </template>
+        </vs-table>
+      </div>
+    </vs-col>
+  </vs-row>
 </template>
 
 <script>
-    import Axios from "axios"
+    import axios from "axios"
+    import FormCiudad from "../Creacion/FormCiudad";
     export default {
-        name: "Ciudad",
+        name: "Jugador",
+        components:{FormCiudad},
         data(){
             return{
                 ciudades: null,
-                users:[
-                    {
-                        "nombre": "La Ceiba",
-                    },
-                    {
-                        "nombre": "Tegucigalpa",
-                    },
-                    {
-                        "nombre": "San Pedro Sula",
-                    },
-                ],
+                nombre: null,
                 popupActivo:false,
-                popupActivo1:false,
+                popupActivo1:false
             }
         },
-        methods: {
-            CargarCiudades(){
-                Axios.get("http://134.209.172.114/ciudades/").then(
-                    res => (
-                        this.ciudades = res.data
-                    )
+        mounted() {
+            axios.get('http://134.209.172.114/api/ciudades/').then(
+                res =>(
+                    this.ciudades = res.data
                 )
-            }
-        },
-    mounted() {
-        this.CargarCiudades();
-    }
+            )
+        }
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus">
+  .cardx
+    margin 15px
+  .default-input
+    .inputx
+      margin 5px
+      margin-top 15px
+  .contenedor
+    display flex
+    flex-direction row
+  .tarjetas
+    padding 30px
 </style>
