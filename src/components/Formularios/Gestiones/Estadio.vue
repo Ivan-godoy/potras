fd995fe49f23d3346bac715ab2dbbe852f1c594b
EdgardoArgueta29 <19992909tato>
2019-12-13 02:23:24 -0600

    Tabla Estadios en Gestiones

--- src/components/Formularios/Gestiones/Estadio.vue
+++ src/components/Formularios/Gestiones/Estadio.vue
@@ -1,79 +1,106 @@
-<template lang="html">
-  <div>
-    <vs-table max-items="5" pagination :data="estadios">
-      <template slot="header">
-        <h3>
-          Estadios
-        </h3>
-      </template>
-      <template slot="thead">
-        <vs-th>
-          Nombre Estadio
-        </vs-th>
-        <vs-th>
-          Capacidad
-        </vs-th>
-        <vs-th>
-          Ciudad
-        </vs-th>
-      </template>
+<template>
+  <vs-row vs-justify="center">
+    <vs-col type="flex" class="tarjetas" vs-justify="center" vs-align="center" vs-w="10">
+      <div>
+        <vs-table stripe :data="estadios">
+          <template slot="header">
+            <h3>
+              Estadios
+            </h3>
+          </template>
+          <template slot="thead">
+            <vs-th>
+              Nombre
+            </vs-th>
 
-      <template>
-        <vs-tr :key="indextr" v-for="(indextr) in estadios" >
-          <vs-td :data="indextr.nombre">
-            {{indextr.nombre}}
-          </vs-td>
+            <vs-th>
+              Capacidad
+            </vs-th>
 
-          <vs-td :data="indextr.capacidad">
-            {{indextr.capacidad}}
-          </vs-td>
+            <vs-th>
+              Ciudad
+            </vs-th>
 
-          <vs-td :data="indextr.ciudad">
-            {{indextr.ciudad}}
-          </vs-td>
-        </vs-tr>
-      </template>
-    </vs-table>
-  </div>
+            <vs-th>
+              Acciones
+            </vs-th>
+          </template>
+          <template slot-scope="{data}">
+            <vs-tr :key="indextr" v-for="(indextr) in estadios" >
+              <vs-td :data="indextr.nombre">
+                {{indextr.nombre}}
+              </vs-td>
+
+              <vs-td :data="indextr.capacidad">
+                {{indextr.capacidad}}
+              </vs-td>
+
+              <vs-td :data="indextr.ciudad">
+                {{indextr.ciudad.nombre}}
+              </vs-td>
+
+              <vs-td>
+                <vs-row vs-w="12">
+                  <div>
+                    <vs-col :key="index" vs-type="flex" vs-justify="center" vs-align="center" vs-w="10">
+                      <vs-button @click="popupActivo=true"  vs-type="gradient" size="medium" color="success" icon="create" style="margin-right: 10px"></vs-button>
+                      <vs-popup class="gestion_Estadios"  title="Editar EStadios" :active.sync="popupActivo">
+                        <div class="contenedor">
+                          <FormEstadio titulo="Formulario EStadio" :id_equipo="id"/>
+                        </div>
+                      </vs-popup>
+                      <vs-button @click="popupActivo1=true" vs-type="flat" size="medium" color="danger" icon="delete_sweep"></vs-button>
+                      <vs-popup class="gestion_Estadios"  title="Eliminar Estadios" :active.sync="popupActivo1">
+                        <p>Hola Guapo, ¿vas a eliminarme?</p>
+                      </vs-popup>
+                    </vs-col>
+                  </div>
+                </vs-row>
+              </vs-td>
+            </vs-tr>
+          </template>
+        </vs-table>
+      </div>
+    </vs-col>
+  </vs-row>
 </template>
 
 <script>
+    import axios from "axios"
+    import FormEstadio from "../Creacion/FormEstadio";
     export default {
-        name: "Estadio",
+        name: "Jugador",
+        components:{FormEstadio},
         data(){
             return{
-                estadios: null,
-                users:[
-                    {
-                        "nombre": "Estadio Ceibeño",
-                        "capacidad": "35,000 personas",
-                        "ciudad": "La Ceiba",
-                    },
-                    {
-                        "nombre": "Estadio Ceibeño",
-                        "capacidad": "35,000 personas",
-                        "ciudad": "La Ceiba",
-                    },
-                ],
+                estadio: null,
+                nombre: null,
+                capacidad: null,
+                ciudad: null,
                 popupActivo:false,
                 popupActivo1:false
             }
         },
-        methods:{
-            CargarEstadios(){
-                Axios.get("http://134.209.172.114/estadios/").then(
-                    res => (
-                        this.estadios = res.data
-                    )
-                )
-            }
-        },
         mounted() {
-            this.CargarEstadios();
+            axios.get('http://134.209.172.114/api/estadios/').then(
+                res =>(
+                    this.estadios = res.data
+                )
+            )
         }
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus">
+  .cardx
+    margin 15px
+  .default-input
+    .inputx
+      margin 5px
+      margin-top 15px
+  .contenedor
+    display flex
+    flex-direction row
+  .tarjetas
+    padding 30px
 </style>
