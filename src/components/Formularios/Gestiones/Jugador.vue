ef74d7bf364cc0a9edcbf27955725190b621dce3
EdgardoArgueta29 <19992909tato>
2019-12-13 00:42:32 -0600

    Tabla Jugadores en Gestiones y Activacion del boton de modificar

--- src/components/Formularios/Gestiones/Jugador.vue
+++ src/components/Formularios/Gestiones/Jugador.vue
@@ -2,7 +2,7 @@
     <vs-row vs-justify="center">
         <vs-col type="flex" class="tarjetas" vs-justify="center" vs-align="center" vs-w="10">
             <div>
-                <vs-table :data="users">
+                <vs-table stripe :data="jugadores">
                     <template slot="header">
                         <h3>
                             Jugadores
@@ -33,37 +33,43 @@
                         <vs-th>
                             Posición
                         </vs-th>
+                      <vs-th>
+                            Dorsal
+                        </vs-th>
                         <vs-th>
                             Acciones
                         </vs-th>
                     </template>
                     <template slot-scope="{data}">
-                        <vs-tr :key="indextr" v-for="(tr, indextr) in data" >
-                            <vs-td :data="data[indextr].nombre">
-                                {{data[indextr].nombre}}
+                        <vs-tr :key="indextr" v-for="(indextr) in jugadores" >
+                            <vs-td :data="indextr.nombre">
+                                {{indextr.jugador.nombre}}
                             </vs-td>
 
-                            <vs-td :data="data[indextr].nacimiento">
-                                {{data[indextr].fecha_nacimiento}}
+                            <vs-td :data="indextr.nacimiento">
+                                {{indextr.jugador.fecha_nacimiento}}
                             </vs-td>
-                            <vs-td :data="data[indextr].nacionalidad">
-                                {{data[indextr].nacionalidad}}
-                            </vs-td>
-
-                            <vs-td :data="data[indextr].nacionalidad">
-                                {{data[indextr].lugar_nacimiento}}
+                            <vs-td :data="indextr.nacionalidad">
+                                {{indextr.jugador.nacionalidad}}
                             </vs-td>
 
-                            <vs-td :data="data[indextr].peso">
-                                {{data[indextr].peso}}
+                            <vs-td :data="indextr.nacionalidad">
+                                {{indextr.jugador.lugar_nacimiento}}
                             </vs-td>
-                            <vs-td :data="data[indextr].estatura">
-                                {{data[indextr].estatura}}
+
+                            <vs-td :data="indextr.peso">
+                                {{indextr.jugador.peso}}
                             </vs-td>
-                            <vs-avatar :badge="tr.id" size="50px"
+                            <vs-td :data="indextr.estatura">
+                                {{indextr.jugador.estatura}}
+                            </vs-td>
+                            <vs-avatar :badge="id" size="50px"
                                        :src="`https://thumbs.dreamstime.com/z/silueta-de-un-jugador-f%C3%BAtbol-del-atleta-que-juega-con-una-bola-en-108740657.jpg`"/>
-                            <vs-td :data="data[indextr].posicion">
-                                {{data[indextr].posicion}}
+                            <vs-td :data="indextr.posicion">
+                                {{indextr.jugador.posicion}}
+                            </vs-td>
+                          <vs-td :data="indextr.dorsal">
+                                {{indextr.dorsal}}
                             </vs-td>
                             <vs-td>
                                 <vs-row vs-w="12">
@@ -71,7 +77,9 @@
                                         <vs-col :key="index" vs-type="flex" vs-justify="center" vs-align="center" vs-w="10">
                                             <vs-button @click="popupActivo=true"  vs-type="gradient" size="medium" color="success" icon="create" style="margin-right: 10px"></vs-button>
                                             <vs-popup class="gestion_Jugador"  title="Editar Jugador" :active.sync="popupActivo">
-                                                <p>Hola Guapo</p>
+                                              <div class="contenedor">
+                                                <FormJugador titulo="Formulario Jugador" :id_equipo="id"/>
+                                              </div>
                                             </vs-popup>
                                             <vs-button @click="popupActivo1=true" vs-type="flat" size="medium" color="danger" icon="delete_sweep"></vs-button>
                                             <vs-popup class="gestion_Jugador"  title="Eliminar Jugador" :active.sync="popupActivo1">
@@ -90,39 +98,48 @@
 </template>
 
 <script>
+  import axios from "axios"
+  import FormJugador from "../Creacion/FormJugador";
     export default {
         name: "Jugador",
+        components:{FormJugador},
         data(){
             return{
-                users:[
-                    {
-                        "nombre": "Oliver",
-                        "fecha_nacimiento": "2 de abril 1997",
-                        "nacionalidad": "Hondureña",
-                        "lugar_nacimiento": "La Ceiba",
-                        "peso": "209 lbs",
-                        "estatura": "1.86",
-                        "posicion": "Delantero",
-                    },
-                    {
-                        "nombre": "Marcelo",
-                        "fecha_nacimiento": "3 de abril 1983",
-                        "nacionalidad": "Brasileña",
-                        "lugar_nacimiento": "Brasilia",
-                        "peso": "180 lbs",
-                        "estatura": "1.77",
-                        "posicion": "Lateral",
-                    },
-                ],
+                jugadores: null,
+                nombre: null,
+                fecha_nacimeinto: null,
+                lugar_nacimeinto: null,
+                peso: null,
+                estatura: null,
+                imagen: null,
+                posicion: null,
+                dorsal: null,
                 popupActivo:false,
                 popupActivo1:false
             }
+        },
+        mounted() {
+            axios.get('http://134.209.172.114/api/jugadores/').then(
+                res =>(
+                    this.jugadores = res.data
+                )
+            )
         }
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus">
+  .cardx
+    margin 15px
+  .default-input
+    .inputx
+      margin 5px
+      margin-top 15px
+  .contenedor
+    display flex
+    flex-direction row
+  .tarjetas
+    padding 30px
 </style>
 
 
