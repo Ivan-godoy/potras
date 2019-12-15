71253953d41063f881a7966ddd8b2df155c5356e
Ivan-godoy <ivangodoy24@gmail.com>
2019-12-14 21:05:21 -0600

    Formulario de Arbitro validado y capaz de guardar datos

--- src/components/Formularios/Creacion/Arbitro.vue
+++ src/components/Formularios/Creacion/Arbitro.vue
@@ -1,10 +1,19 @@
 <template lang="html">
+    <vs-row vs-justify="center">
+        <vs-col type="flex" class="tarjetas" vs-justify="center" vs-align="center" vs-w="7">
+            <FormArbitro/>
+        </vs-col>
 
-
+    </vs-row>
 </template>
 
 <script>
+    import FormArbitro from "./FormArbitro";
+    export default {
+        name: "Arbitro",
+        components:{FormArbitro},
 
+    }
 </script>
 
 <style scoped>
