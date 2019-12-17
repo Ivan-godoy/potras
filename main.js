265b264451e3035d0ecee781a2f8240eae00b315
Ivan-godoy <ivangodoy24@gmail.com>
2019-12-16 20:38:54 -0600

    Modificaciones en Temporada

--- main.js
+++ main.js
@@ -4,12 +4,10 @@ function createWindow () {
     // Create the browser window.
     let win = new BrowserWindow({
         width: 800,
-        height: 800,
-        fullscreen: true
+        height: 800
     });
 
     // and load the index.html of the app.
-    win.setFullScreen(true);
     win.loadFile('dist/index.html')
 }
 
